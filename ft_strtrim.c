1872604c70b487e50d70853b3e11a400e8cb7db5
Molasz <martiolasz@gmail.com>
2024-01-10 11:01:31 +0100

    refactor strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,25 +6,11 @@
 /*   By: molasz-a <molasz-a@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 16:10:09 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/01/10 00:41:46 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/01/10 11:00:31 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	c_in_set(char c, char const *set)
-{
-	size_t	i;
-
-	i = 0;
-	while (set[i])
-	{
-		if (set[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
 
 char	*ft_strtrim(char const *s, char const *set)
 {
@@ -34,9 +20,9 @@ char	*ft_strtrim(char const *s, char const *set)
 
 	start = 0;
 	end = ft_strlen(s);
-	while (s[start] && c_in_set(s[start], set))
+	while (s[start] && ft_strchr(set, s[start]))
 		start++;
-	while (end > start && c_in_set(s[end - 1], set))
+	while (end > start && ft_strchr(set, s[end - 1]))
 		end--;
 	str = ft_calloc(sizeof (char), end - start + 1);
 	if (!str)
